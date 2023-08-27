355f4b98e7bdd6b7eea1501d78bc926fc6003c76
AbdelhakMHD <abdo.m@caspiands.com>
2023-08-27 09:15:31 +0100

    add Navbar Comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,32 @@
+import Nav from "./components/Nav"
+import { CustomerReviews, Footer, Hero, PopularProducts, Services, SpecialOffer, Subscribe, SuperQuality } from "./sections"
+
 const App = () => (
   <main className=" relative">
-    Nav
+    <Nav />
     <section className=" xl:padding-1 wide:padding-r padding-b">
-      hero
+      <Hero />
     </section>
     <section className="padding">
-      Popular Products
-
+      <PopularProducts />
     </section>
     <section className="padding">
-      Super Quelity
-
+      <SuperQuality />
     </section>
     <section className="padding-x py-10">
-      Services
-
+      <Services />
     </section>
     <section className="padding">
-      Spacial Offers
+      <SpecialOffer />
     </section>
     <section className=" bg-pale-blue padding">
-      Customer Reviews
+      <CustomerReviews />
     </section>
     <section className="padding-x sm:py-32 py-16 w-full">
-      Subscribe
+      <Subscribe />
     </section>
     <section className=" bg-black padding-x padding-t pb-8">
-      Fotter
+      <Footer />
     </section>
   </main>
 )
